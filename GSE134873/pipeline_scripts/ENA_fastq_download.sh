d8218c34042d45dbda5640126adc5002cf0dfea9
Shruti Singh Kakan <singhkak@sh04-ln05.stanford.edu>
2025-03-19 14:27:20 -0700

    fastQC and trimgalore slurm scripts for all folders

---
+++ GSE134873/pipeline_scripts/ENA_fastq_download.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/004/SRR9833664/SRR9833664_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/000/SRR9833670/SRR9833670_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/001/SRR9833671/SRR9833671_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/003/SRR9833663/SRR9833663_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/007/SRR9833667/SRR9833667_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/008/SRR9833668/SRR9833668_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/009/SRR9833669/SRR9833669_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/008/SRR9833668/SRR9833668_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/009/SRR9833669/SRR9833669_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/000/SRR9833670/SRR9833670_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/006/SRR9833666/SRR9833666_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/005/SRR9833665/SRR9833665_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/002/SRR9833662/SRR9833662_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/006/SRR9833666/SRR9833666_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/001/SRR9833671/SRR9833671_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/002/SRR9833672/SRR9833672_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/004/SRR9833664/SRR9833664_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/003/SRR9833663/SRR9833663_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/007/SRR9833667/SRR9833667_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/002/SRR9833672/SRR9833672_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/005/SRR9833665/SRR9833665_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR983/002/SRR9833662/SRR9833662_2.fastq.gz
